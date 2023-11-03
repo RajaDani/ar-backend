98f0036af313833d986ecd89a728853af2d780cc
DanishRaja889 <101696808+DanishRaja889@users.noreply.github.com>
2023-11-03 19:20:22 +0500

    ads

--- src/globals/config.js
+++ src/globals/config.js
@@ -7,9 +7,9 @@ const env = nconf.get("benv");
 
 console.log(
   "Loaded Config File >",
-  path.join(__dirname, "../env", `${env}.json`)
+  path.join(__dirname, "../env", `local.json`)
 );
 
-nconf.file({ file: path.join(__dirname, "../env", `${env}.json`) });
+nconf.file({ file: path.join(__dirname, "../env", `local.json`) });
 
 module.exports = nconf;
